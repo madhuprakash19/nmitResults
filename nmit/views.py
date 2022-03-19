f25e401f97508fe40f684c32400bee23d2cbc634
madhuprakash19 <madhutp11@gmail.com>
2022-03-19 17:06:27 +0530

    topper feature

--- nmit/views.py
+++ nmit/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from nmit.models import student,gpa
 # Create your views here.
 def home(request):
-    a = list(gpa.objects.filter(branch = 'B.E - IS, Sem 5'))
-    return render(request,'home.html',{'a':a})
+    topper5 = list(gpa.objects.order_by('-sgpa')[:10:1])
+    is5 = list(gpa.objects.filter(branch = 'B.E - IS, Sem 5').order_by('-sgpa')[:10:1])
+    cs = list(gpa.objects.filter(branch = 'B.E - CS, Sem 5').order_by('-sgpa')[:10:1])
+    ec = list(gpa.objects.filter(branch = 'B.E - EC, Sem 5').order_by('-sgpa')[:10:1])
+    me = list(gpa.objects.filter(branch = 'B.E - ME, Sem 5').order_by('-sgpa')[:10:1])
+    ae = list(gpa.objects.filter(branch = 'B.E - AE, Sem 5').order_by('-sgpa')[:10:1])
+    ee = list(gpa.objects.filter(branch = 'B.E - EE, Sem 5').order_by('-sgpa')[:10:1])
+    return render(request,'home.html',{'topper5':topper5,'is5':is5,'cs':cs,'ec':ec,'me':me,'ae':ae,'ee':ee})
